64430da56d6134bd9d2663d4fe2be954c6c8725c
Ignat Mihai <mihai.ignat@assist.ro>
2017-04-20 13:55:15 +0300

    travis test 1.3

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -1,9 +1,13 @@
 package com.example;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
 
 /**
  * Created by mihai on 20.04.2017.
@@ -13,8 +17,8 @@ public class UserController {
 
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public String getString(){
-        return "this is a string";
+    public ResponseEntity getString() {
+        return ResponseEntity.status(HttpStatus.OK).body("this is a string");
     }
 
 }
--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -3,12 +3,17 @@ package com.example;
 import org.junit.Test;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.awt.image.RescaleOp;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
@@ -16,7 +21,11 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Autowired
     UserController userController;
@@ -28,6 +37,14 @@ public class DemoApplicationTests {
         assertTrue(a.equals(b));
         assertFalse(a == b);
         assertEquals(a, b);
+    }
+
+    @Test
+    public void testTestTest() throws Exception {
+        this.mockMvc.perform(get("/get"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("this is a string"));
     }
 
 }
